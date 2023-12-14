348368c05023b70390ff181738037748f46e23d8
Brian Ortiz <b.ortiz.a95@gmail.com>
2023-12-14 13:17:02 -0500

    Module Search Path file completed

---
+++ Modules/ModuleSearchPath.py
@@ -0,0 +1,14 @@
+# I have imported the sales module
+# When python sees this, it will look for a file called sales.py in the current directory
+# If it doesnt find this file, it will look in a bunch of predefined directories that come with python installation
+
+
+# import sales
+
+# we have a built in module called path
+import sys
+print(sys.path)
+
+# line 10 will print an array of strings
+# the first element in this array represents the current folder
+# '/Users/ozi-b/Documents/Coding/CodeWithMosh/Complete-Python-Mastery/Python/Modules'
